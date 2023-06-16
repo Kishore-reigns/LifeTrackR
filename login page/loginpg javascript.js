b8686e503d53518fd7a3c441d8571e0aea1698f8
Paramesh612 <136327127+Paramesh612@users.noreply.github.com>
2023-06-16 22:44:03 +0530

    changes to login  page

--- login page/loginpg javascript.js
+++ login page/loginpg javascript.js
@@ -1,3 +1,26 @@
 function move_up(num){
-    document.getElementById("holder" +num).style.cssText="transform: translateY(-5px);"
+    document.getElementById("username" +num).style.cssText="transform: translateY(-5px);"
+}
+function disp_reg(){
+    document.getElementById("loginbox").style.cssText="display:none;";
+    document.getElementById("registerbox").style.cssText="display:block;";
+}
+function disp_log(){
+    document.getElementById("loginbox").style.cssText="display:block;";
+    document.getElementById("registerbox").style.cssText="display:none;";
+}
+function toggle_password(n)
+{
+    var pass=document.getElementById("pass"+n);
+    if(pass.type==="password")
+    {
+        pass.type="text"
+    }
+    else{
+        pass.type="password"
+    }
+}
+function gone(el)
+{
+    document.getElementById(el).style.display="none";
 }
