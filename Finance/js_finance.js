8dbf4fdb8048373d5eb5bd569c01c4a631cd178b
Paramesh612 <136327127+Paramesh612@users.noreply.github.com>
2023-07-10 23:10:43 +0530

    o

--- Finance/js_finance.js
+++ Finance/js_finance.js
@@ -1,4 +1,14 @@
-function addexpense(){
+var newexpcount=0;
+
+function openpopup(){
+    document.getElementById("popup").style.display="block";
+    document.getElementById("blursheet").style.display="block";
+    create_new_element();
+}
+function create_new_element(){
+    newexpcount++;
     const exp=document.createElement("div");
-        exp.setAttribute("class","expense");
+    exp.setAttribute("class","expense");
+    exp.setAttribute("id","exp"+newexpcount);
+    document.getElementById("exp"+newexpcount).innerHTML="AAAAAAAAA";
 }
