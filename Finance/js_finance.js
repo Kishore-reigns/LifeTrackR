387fb0089186b5e142d472cef5a2b76bb8676d46
Paramesh612 <136327127+Paramesh612@users.noreply.github.com>
2023-07-11 00:02:33 +0530

    aaaaaaaaaaaaaaaaaaaaaaa

--- Finance/js_finance.js
+++ Finance/js_finance.js
@@ -3,12 +3,18 @@ var newexpcount=0;
 function openpopup(){
     document.getElementById("popup").style.display="block";
     document.getElementById("blursheet").style.display="block";
-    create_new_element();
+}
+function closepopup(){
+    document.getElementById("popup").style.display="none";
+    document.getElementById("blursheet").style.display="none";
 }
 function create_new_element(){
     newexpcount++;
-    const exp=document.createElement("div");
-    exp.setAttribute("class","expense");
-    exp.setAttribute("id","exp"+newexpcount);
-    document.getElementById("exp"+newexpcount).innerHTML="AAAAAAAAA";
+    const exp=document.createElement("p");
+    const division=document.getElementById("skeleton");
+    division.appendChild(exp);
+    exp.id="exp"+newexpcount;
+    document.getElementById("exp"+newexpcount).innerHTML='<div class="expense"><div class="spend"><p style="font-size: 30px; margin-bottom: 0px">₹</p><p class="num">24</p></div><div><h2>Title</h2><p>date dd/mm/yy</p><p>Place</p></div></div>';
+    if(newexpcount!=1 ) {document.getElementById("skeleton").style.height=(770+(newexpcount*50))+"px";}
+    else {document.getElementById("skeleton").style.height="620px";}
 }
