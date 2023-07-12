1ae2b39d1a80f9ec661feae8d026c1e617ed09ee
Kishore-reigns <112459807+Kishore-reigns@users.noreply.github.com>
2023-07-12 17:38:19 +0530

    fitnesssssss

--- Fitness/fitnessjs.js
+++ Fitness/fitnessjs.js
@@ -1,5 +1,6 @@
 var det = [0,1,2,3];
 
+var BMI = 0 ; 
 
 function movetoheight(){
     document.getElementById("enterweight").style.display = "block";
@@ -20,6 +21,10 @@ function validateBmi(){
     h /= 100  ; 
     w = parseInt(w);
     var bmi = w/(h**2);
+    //////////////////////////////// local stoarage for bmi value /////////////////////////////
+    localStorage.setItem(bmi, BMI);
+    BMI = bmi ;
+    //////////////////////////////////////////////////////////////////////////////////////////
     var description ; 
     if(isNaN(bmi)){
         console.log("error in taking value ");
@@ -76,5 +81,133 @@ function openform(){
    
     document.getElementById("fitnessformdiv").style.filter = 'none';
     
+
+}
+
+function workoutVideos(){
+    if (BMI < 18.5){
+
+        console.log(BMI + " lightweight workout videos opened ");
+
+        document.getElementById("lightWeight").style.display = 'block';
+        document.getElementById("OverWeight").style.display = 'none';
+        document.getElementById("obese").style.display = 'none';
+        document.getElementById("NoramlWeight").style.display = 'none';
+    
+        document.getElementById("NoramlWeightDiet").style.display = 'none';
+        document.getElementById("obeseDiet").style.display = 'none';
+        document.getElementById("overWeightDiet").style.display ='none';
+        document.getElementById("lightweightdiet").style.display = 'none';
+       
+    }
+    //normal weight 
+    else if (18.5 >= BMI && BMI < 25){
+
+        console.log(BMI + "normal workout videos opened ");
+
+        document.getElementById("lightWeight").style.display = 'none';
+        document.getElementById("OverWeight").style.display = 'none';
+        document.getElementById("obese").style.display = 'none';
+        document.getElementById("NoramlWeight").style.display = 'block';
+    
+        document.getElementById("NoramlWeightDiet").style.display = 'none';
+        document.getElementById("obeseDiet").style.display = 'none';
+        document.getElementById("overWeightDiet").style.display ='none';
+        document.getElementById("lightweightdiet").style.display = 'none';
+    }
+    //overweight
+    else if (25 >= BMI && BMI < 30){
+
+        console.log(BMI + "overweight workout videos opened ");
+
+        document.getElementById("lightWeight").style.display = 'none';
+        document.getElementById("OverWeight").style.display = 'block';
+        document.getElementById("obese").style.display = 'none';
+        document.getElementById("NoramlWeight").style.display = 'none';
+    
+        document.getElementById("NoramlWeightDiet").style.display = 'none';
+        document.getElementById("obeseDiet").style.display = 'none';
+        document.getElementById("overWeightDiet").style.display ='none';
+        document.getElementById("lightweightdiet").style.display = 'none';
+    }
+    //obese
+    else{
+
+        console.log(BMI + "obese workout videos opened ");
+
+        document.getElementById("lightWeight").style.display = 'none';
+        document.getElementById("OverWeight").style.display = 'none';
+        document.getElementById("obese").style.display = 'block';
+        document.getElementById("NoramlWeight").style.display = 'none';
+    
+        document.getElementById("NoramlWeightDiet").style.display = 'none';
+        document.getElementById("obeseDiet").style.display = 'none';
+        document.getElementById("overWeightDiet").style.display ='none';
+        document.getElementById("lightweightdiet").style.display = 'none';
+    }
+}
+
+function Diet(){
+
+    if (BMI < 18.5){
+
+        console.log(BMI + "lightweight diet videos opened ");
+
+        document.getElementById("lightWeight").style.display = 'none';
+        document.getElementById("OverWeight").style.display = 'none';
+        document.getElementById("obese").style.display = 'none';
+        document.getElementById("NoramlWeight").style.display = 'none';
+    
+        document.getElementById("NoramlWeightDiet").style.display = 'none';
+        document.getElementById("obeseDiet").style.display = 'none';
+        document.getElementById("overWeightDiet").style.display ='none';
+        document.getElementById("lightweightdiet").style.display = 'block';
+       
+    }
+    //normal weight 
+    else if (18.5 >= BMI && BMI < 25){
+
+        console.log(BMI + "normal diet videos opened ");
+
+        document.getElementById("lightWeight").style.display = 'none';
+        document.getElementById("OverWeight").style.display = 'none';
+        document.getElementById("obese").style.display = 'none';
+        document.getElementById("NoramlWeight").style.display = 'none';
+    
+        document.getElementById("NoramlWeightDiet").style.display = 'block';
+        document.getElementById("obeseDiet").style.display = 'none';
+        document.getElementById("overWeightDiet").style.display ='none';
+        document.getElementById("lightweightdiet").style.display = 'none';
+    }
+    //overweight
+    else if (25 >= BMI && BMI < 30){
+
+        console.log(BMI + "overweight diet videos opened ");
+
+        document.getElementById("lightWeight").style.display = 'none';
+        document.getElementById("OverWeight").style.display = 'none';
+        document.getElementById("obese").style.display = 'none';
+        document.getElementById("NoramlWeight").style.display = 'none';
+    
+        document.getElementById("NoramlWeightDiet").style.display = 'none';
+        document.getElementById("obeseDiet").style.display = 'none';
+        document.getElementById("overWeightDiet").style.display ='block';
+        document.getElementById("lightweightdiet").style.display = 'none';
+    }
+    //obese
+    else{
+
+        console.log(BMI + "obese diet videos opened ");
+
+        document.getElementById("lightWeight").style.display = 'none';
+        document.getElementById("OverWeight").style.display = 'none';
+        document.getElementById("obese").style.display = 'none';
+        document.getElementById("NoramlWeight").style.display = 'none';
+    
+        document.getElementById("NoramlWeightDiet").style.display = 'none';
+        document.getElementById("obeseDiet").style.display = 'block';
+        document.getElementById("overWeightDiet").style.display ='none';
+        document.getElementById("lightweightdiet").style.display = 'none';
+    }
 
 }
